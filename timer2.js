4ac67cf0a327ca43df08dd69d013928b192380ec
Carrie G <carr1eg@Hotmail.com>
2021-08-24 21:02:55 +0000

    implemented interactive timer

---
+++ timer2.js
@@ -0,0 +1,16 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+process.stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+  if (key === 'b') process.stdout.write('\x07');
+  if (/\d/.test(key)) {
+    console.log(`setting timer for ${key} seconds..`);
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, key * 1000);
+  }
+});
